f6cc66f2e14d283da0884a96510f2bf247894ea7
dinosauce-285 <thanhbualqt@gmail.com>
2021-11-18 16:51:45 +0700

    check

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ let un = document.getElementById("username");
 let ps = document.getElementById("pass");
 let rps = document.getElementById("repass");
 let rbtn = document.getElementById("regisbtn");
-function test() {
+
+function dangKi() {
   if (un.value == false) {
     const wrong = document.createElement("div");
 
@@ -20,7 +21,20 @@ function test() {
     return;
   }
   if (rps.value == ps.value) {
-    localStorage.setItem(un.value, ps.value);
+    let users = [];
+    let newUser = {
+      username: un.value,
+      password: ps.value,
+    };
+    users = JSON.parse(localStorage.getItem("users"));
+    if (users == null) {
+      users = [];
+    }
+    users.push(newUser);
+
+    let json = JSON.stringify(users);
+
+    localStorage.setItem("users", json);
   }
   if (rps.value != ps.value) {
     const wrong = document.createElement("div");
@@ -37,6 +51,25 @@ function test() {
     wrongrps.style.justifyContent = "center";
     wrongrps.style.position = "absolute";
   }
-  
 }
-rbtn.onclick = test;
+
+rbtn.onclick = dangKi;
+
+let lbtn = document.getElementById('loginBtn');
+function logIn() {
+  let iUsername = document.getElementById("nhapUserName");
+  let iPassword = document.getElementById("nhapPass");
+  let data = localStorage.getItem("users");
+  let aData = JSON.parse(data);
+  function check (usernameAndPassword) {
+    if (usernameAndPassword.username == iUsername.value) {
+      let checkData = aData.find(function () {
+        return usernameAndPassword.username == iUsername.value ;
+      });
+       console.log(checkData);
+    }
+ 
+  }
+check (usernameAndPassword);
+}
+lbtn.onclick = logIn
